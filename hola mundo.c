71510a96f98cc2027cbcb23d838aa808185a3ba8
LuisFer98 <42361737+LuisFer98@users.noreply.github.com>
2018-08-13 20:38:07 -0500

    Add files via upload

---
+++ hola mundo.c
@@ -0,0 +1,21 @@
+/** main.c - Este es un archivo Hola Mundo
+* incluido en este curso --solo para evitar 
+* la maldici\'on del programador--. En este curso, 
+* usted aprender \a usar correctamente muchos de 
+*
+*/
+#include <stdio.h> /* printf(char*,...)
+Este es un ejemplo de un ejemplo de bloque */
+
+
+#include <stdlib.h> //
+
+int main(int argc, char *argv[])
+{
+  putchar('A');
+  putchar('\n');
+  printf("hola mundo c!!! \n ");
+  
+  system("PAUSE");	
+  return 0;
+}
